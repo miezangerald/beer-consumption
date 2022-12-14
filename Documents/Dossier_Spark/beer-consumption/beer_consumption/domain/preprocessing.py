e49579757d4b0a4f9c26b081d57dd91e49c891d0
miezangerald <gerald.miezan2016@gmail.com>
2022-12-14 13:49:19 +0000

    quatriem push

--- Documents/Dossier_Spark/beer-consumption/beer_consumption/domain/preprocessing.py
+++ Documents/Dossier_Spark/beer-consumption/beer_consumption/domain/preprocessing.py
@@ -1,5 +1,6 @@
 ## 1. Which brewery produces the strongest beers by ABV%?
-
 def meilleure_brasserie(data):
     max_abv =  data["beer_abv"].max()
     return data[data['beer_abv']==max_abv]["brewery_name"]
+
+## 2. If you had to pick 3 beers to recommend using only this data, which would you pick ?
